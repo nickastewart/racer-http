42dc9c157d9526bc45a60ee5de49b79da3712589
nickstewart <nick.stewart1993@gmail.com>
2025-09-21 10:50:39 +0100

    parse file and return json

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	racer "github.com/nickastewart/racer-parser"
 )
 
 func main() {
 	router := gin.Default()
 	router.GET("/parse", func(c *gin.Context){
 		c.JSON(200, gin.H{
-			"message": "parse",
+			"message": racer.Parse("/Users/nickstewart/results/2024-07-16-Milton-Keynes.eml"),
 		})
 	})
 
