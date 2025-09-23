63dfaa8e27fc693e0a68d45f01c69f7f627fb8f6
nickstewart <nick.stewart1993@gmail.com>
2025-09-23 21:20:36 +0100

    get hardcoded user from sqlite db

--- main.go
+++ main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+	_ "embed"
 	"github.com/gin-gonic/gin"
 	racer "github.com/nickastewart/racer-parser"
+	"log"
+	_ "modernc.org/sqlite"
+	"racer_http/racer-http-db"
 )
 
 func main() {
+
+	ctx := context.Background()
+
+	db, err := sql.Open("sqlite", "/Users/nickstewart/sqlite/racer.db")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+
+	queries := racer_http.New(db)
+
+	user, err := queries.GetUser(ctx, int64(1))
+
+	if err != nil {
+		log.Panic(err)
+	}
 	router := gin.Default()
+
 	router.GET("/parse", func(c *gin.Context) {
 		filePath := c.Query("file")
 		c.JSON(200, gin.H{
@@ -14,5 +37,13 @@ func main() {
 		})
 	})
 
+	router.GET("/user", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{
+			"User": user,
+		})
+	})
+
 	router.Run()
 }
+
--- migrations/20250921192949_create_event_table.sql
+++ migrations/20250921192949_create_event_table.sql
@@ -3,10 +3,9 @@
 CREATE TABLE IF NOT EXISTS event (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   location_id INTEGER NOT NULL,
-  type VARCHAR(50) NOT NULL,
+  type VARCHAR(100) NOT NULL,
   date TEXT NOT NULL,
   total_drivers INTEGER NOT NULL,
-  car_type VARCHAR(50),
   FOREIGN KEY (location_id) REFERENCES location (id)
 );
 
