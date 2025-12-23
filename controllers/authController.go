49d644af0b754ba5ce7069a1939742586a171a64
nickstewart <nick.stewart1993@gmail.com>
2025-12-23 14:06:12 +0000

    add signup form with templ

--- controllers/authController.go
+++ controllers/authController.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"racer_http/models"
 	"racer_http/repository"
 	"racer_http/sqlite/entities"
 	"racer_http/templates"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -25,6 +23,15 @@ type LoginForm struct {
 	Password string `form:"password"`
 }
 
+type SignupForm struct {
+	Email                string `form:"email"`
+	ConfirmationEmail    string `form:"email-confirm"`
+	FirstName            string `form:"first-name"`
+	LastName             string `form:"last-name"`
+	Password             string `form:"password"`
+	ConfirmationPassword string `form:"password-confirm"`
+}
+
 // TODO: Change to inject a service instead of a repo
 func NewAuthController(userRepository repository.UserRepository) *AuthController {
 	return &AuthController{
@@ -32,31 +39,27 @@ func NewAuthController(userRepository repository.UserRepository) *AuthController
 	}
 }
 
-func (controller *AuthController) CreateUser(c *gin.Context) {
+func (controller *AuthController) Signup(c *gin.Context) {
 	ctx := context.Background()
 
-	var createUserRequest models.CreateUserRequest
+	var signupForm SignupForm
+	c.Bind(&signupForm)
 
-	if err := c.ShouldBindJSON(&createUserRequest); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	userFound, err := controller.UserRepository.GetUserByEmail(ctx, createUserRequest.Email)
+	userFound, err := controller.UserRepository.GetUserByEmail(ctx, signupForm.Email)
 	if userFound.ID != 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "User with this email already exists"})
 		return
 	}
 
-	passwordHash, err := bcrypt.GenerateFromPassword([]byte(createUserRequest.Password), bcrypt.DefaultCost)
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(signupForm.Password), bcrypt.DefaultCost)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	createUserParams := entities.CreateUserParams{
-		FirstName: createUserRequest.FirstName,
-		LastName:  createUserRequest.LastName,
-		Email:     createUserRequest.Email,
+		FirstName: signupForm.FirstName,
+		LastName:  signupForm.LastName,
+		Email:     signupForm.Email,
 		Password:  string(passwordHash),
 	}
 
@@ -68,41 +71,6 @@ func (controller *AuthController) CreateUser(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"data": user})
-}
-
-func (controller *AuthController) Login(c *gin.Context) {
-	ctx := context.Background()
-	var authInput models.AuthInput
-
-	if err := c.ShouldBindJSON(&authInput); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	}
-
-	userFound, _ := controller.UserRepository.GetUserByEmailForLogin(ctx, authInput.Email)
-
-	if userFound.ID == 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
-		return
-	}
-
-	if err := bcrypt.CompareHashAndPassword([]byte(userFound.Password), []byte(authInput.Password)); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
-
-	generateToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"id":  userFound.ID,
-		"exp": time.Now().Add(time.Hour * 24).Unix(),
-	})
-
-	// TODO: Use a real secret from env variables for signing jwt
-	token, err := generateToken.SignedString([]byte("SECRET"))
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "failed to generate token"})
-	}
-
-	c.JSON(200, gin.H{"token": token})
 }
 
 func (controller *AuthController) LoginForm(c *gin.Context) {
@@ -174,66 +142,6 @@ func (controller *AuthController) CheckAccessToken(c *gin.Context) {
 	}
 
 	tokenString := authToken
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-		// TODO: Use a real secret from env variables for signing jwt
-		return []byte("SECRET"), nil
-	})
-
-	if err != nil || !token.Valid {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	claims, ok := token.Claims.(jwt.MapClaims)
-	if !ok {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
-		c.Abort()
-		return
-	}
-
-	if float64(time.Now().Unix()) > claims["exp"].(float64) {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "token expired"})
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	user, _ := controller.UserRepository.GetUserById(ctx, int64(claims["id"].(float64)))
-	if user.ID == 0 {
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	c.Set(
-		"currentUser",
-		user,
-	)
-
-	c.Next()
-}
-
-func (controller *AuthController) CheckAuth(c *gin.Context) {
-	ctx := context.Background()
-
-	authHeader := c.GetHeader("Authorization")
-
-	if authHeader == "" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header is missing"})
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	authToken := strings.Split(authHeader, " ")
-	if len(authToken) != 2 || authToken[0] != "Bearer" {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token format"})
-		c.AbortWithStatus(http.StatusUnauthorized)
-		return
-	}
-
-	tokenString := authToken[1]
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
--- main.go
+++ main.go
@@ -25,13 +25,13 @@ func main() {
 
 	queries := entities.New(db)
 	var userRepository repository.UserRepository = repository.NewUserRepository(queries)
-	var locationRepository repository.LocationRepository = repository.NewLocationRepository(queries)
-	var eventRepository repository.EventRepository = repository.NewEventRepository(queries)
-	var eventResultRepository repository.EventResultRepository = repository.NewEventResultRepository(queries)
+	//var locationRepository repository.LocationRepository = repository.NewLocationRepository(queries)
+	//var eventRepository repository.EventRepository = repository.NewEventRepository(queries)
+	//var eventResultRepository repository.EventResultRepository = repository.NewEventResultRepository(queries)
 
 	authController := controllers.NewAuthController(userRepository)
-	fileUploadController := controllers.NewFileUploadController(userRepository, eventRepository, locationRepository, eventResultRepository)
-	eventController := controllers.NewEventsController(userRepository, eventRepository, locationRepository, eventResultRepository)
+	//	fileUploadController := controllers.NewFileUploadController(userRepository, eventRepository, locationRepository, eventResultRepository)
+	//	eventController := controllers.NewEventsController(userRepository, eventRepository, locationRepository, eventResultRepository)
 
 	if err != nil {
 		log.Panic(err)
@@ -39,15 +39,8 @@ func main() {
 
 	router := gin.Default()
 
-	router.POST("auth/user", authController.CreateUser)
-	router.GET("auth/user", authController.Login)
+	router.POST("/signup", authController.Signup)
 	router.POST("/login", authController.LoginForm)
-
-	// TODO: delete this when a get profile functionlity is implemented
-	router.GET("user", authController.CheckAuth, authController.GetUser)
-
-	router.POST("upload", authController.CheckAuth, fileUploadController.UploadFile)
-	router.GET("events", authController.CheckAuth, eventController.GetEventsByUser)
 
 	// TODO: Add add friend endpoint, need to check auth
 
@@ -58,11 +51,16 @@ func main() {
 
 	router.HTMLRender = &TemplRender{}
 	router.GET("/", authController.CheckAccessToken, func(c *gin.Context) {
-		c.HTML(http.StatusOK, "Home Page", templates.Home())
+		c.HTML(http.StatusOK, "", templates.Home())
 	})
 
 	router.GET("/login", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "Home Page", templates.Login())
+		c.HTML(http.StatusOK, "", templates.Login())
 	})
+
+	router.GET("/signup", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "", templates.Signup())
+	})
+
 	router.Run()
 }
